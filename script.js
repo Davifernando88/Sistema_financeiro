6d81cd8686ceef3312260c51a6594f09e0686013
Usuario <davifernando88@gmail.com>
2026-03-14 11:23:46 -0300

    adicionando menu

--- script.js
+++ script.js
@@ -58,4 +58,40 @@ saldo -= m.valor
 document.getElementById("saldo").innerText = "R$ " + saldo
 
 }
+function fazerLogin(){
 
+let usuario = document.getElementById("usuario").value;
+let senha = document.getElementById("senha").value;
+
+if(usuario === "admin" && senha === "1234"){
+    
+    window.location.href = "index.html";
+
+}else{
+
+    document.getElementById("erro").innerText = "Usuário ou senha inválidos";
+
+}
+
+}
+function abrirCadastro(){
+
+window.location.href = "cadastro.html";
+
+}
+function fazerLogin(){
+
+let usuario = document.getElementById("usuario").value;
+let senha = document.getElementById("senha").value;
+
+if(usuario === "admin" && senha === "1234"){
+
+    window.location.href = "menu.html";
+
+}else{
+
+    document.getElementById("erro").innerText = "Usuário ou senha inválidos";
+
+}
+
+}
